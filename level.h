4d8eb5707927ac1fff2ad86696d866de973d50a1
Luxab <dklug@ucmerced.edu>
2017-04-08 10:15:18 -0700

    Modified background system to use RectangleShape

--- level.h
+++ level.h
@@ -1,5 +1,8 @@
 //  Created by Duncan Klug on 4/7/17.
 //	Lab 02 Spring 2017
+#ifndef LEVEL_H
+#define LEVEL_H
+
 #include <stdlib.h>
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -13,22 +16,27 @@ class Level
 protected:
   int resW, resH;
   RenderWindow *window;
-  Texture *BGTexture;
   int *levelIndex;
   Event *event;
+  RectangleShape background;
 
 public:
   Level()
   {
     resW = 800;
     resH = 600;
+    background.setSize(Vector2f(resW,resH));
+    background.setPosition(0,0);
+    background.setFillColor(*(new Color(255,255,255)));
   }
-  Level(int rw, int rh, RenderWindow &win, Event &ev, int &lin, Texture &tin)
+  Level(int rw, int rh, RenderWindow &win, Event &ev, int &lin)
   {
     resW = rw;
     resH = rh;
+    background.setSize(Vector2f(resW,resH));
+    background.setPosition(0,0);
+    background.setFillColor(*(new Color(255,255,255)));
     window = &win;
-    BGTexture = &tin;
     levelIndex = &lin;
     event = &ev;
   }
@@ -43,3 +51,4 @@ public:
   }
 
 };
+#endif
