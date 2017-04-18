da8d0239d764f0dea27afaef59e247bb2255bd4c
Luxab <dklug@ucmerced.edu>
2017-04-18 15:20:24 -0700

    Initial Commit

---
+++ Player.h
@@ -0,0 +1,35 @@
+//  Created by Duncan Klug on 4/18/17.
+//	Lab 02 Spring 2017
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Player : Sprite
+{
+public:
+  int health; //How much damage can it take
+  int speed; //How fast does it move
+
+  Player(int xin, int yin, Texture &tin, IntRect &rin, int s, int h) : Sprite(tin,rin)
+  {
+    speed = s;
+    health = h;
+    setPosition(xin,yin);
+  }
+  ~Player()
+  {
+
+  }
+
+  void tickMove()
+  {
+    
+    //Move in {direction} at {speed}
+    //setPosition(x,y);
+  }
+
+  bool contains(int mx, int my)
+  {
+    return getTextureRect().contains(mx,my);
+  }
+};
