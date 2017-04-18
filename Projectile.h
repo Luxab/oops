c26c90edb7d2d174f60aa58f67528460c447731e
Luxab <dklug@ucmerced.edu>
2017-04-18 09:07:35 -0700

    Initial Commit

---
+++ Projectile.h
@@ -0,0 +1,34 @@
+//  Created by Duncan Klug on 4/11/17.
+//	Lab 02 Spring 2017
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Projectile : Sprite
+{
+public:
+  int potency; //How much damage does the projectile do
+  int speed; //How fast does the projectile move
+  //TODO: Implement direction the projectile will move in
+  //Position will be measured in floats
+
+  Projectile(int xin, int yin, Texture &tin, IntRect &rin) : Sprite(tin,rin)
+  {
+    setPosition(xin,yin);
+  }
+  ~Projectile()
+  {
+
+  }
+
+  void tickMove()
+  {
+    //Move in {direction} at {speed}
+    //setPosition(x,y);
+  }
+
+  bool contains(int mx, int my)
+  {
+    return m_textureRect.contains(mx,my);
+  }
+};
