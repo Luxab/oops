9898678b7a7e96e9b0ec469e8a7c99d093512bbc
Luxab <duncanklug@outlook.com>
2017-04-30 19:31:39 -0700

    Fixed compiling on Bash for Windows
    Special thanks to @WhipeeDip

--- definitions.h
+++ definitions.h
@@ -1,3 +1,14 @@
-#define int_vec std::vector<int> 
-#define proj_map std::unordered_map<int, Projectile*> 
-#define enemy_map std::unordered_map<int, Enemy*>
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <vector>
+#include <map>
+
+class Projectile;
+class Enemy;
+
+typedef std::vector<int> int_vec;
+typedef std::unordered_map<int, Projectile*> proj_map;
+typedef std::unordered_map<int, Enemy*> enemy_map;
+
+#endif
