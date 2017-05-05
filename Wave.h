1a883b36f15a5e9faab8876ecf8b76ace1e5cbb7
Andrew Morgan <anoa@openmailbox.org>
2017-05-04 20:03:53 -0700

    Add MIT license to beginning of each file.

--- Wave.h
+++ Wave.h
@@ -1,5 +1,31 @@
-//  Created by Duncan Klug on 4/18/17.
-//	Lab 02 Spring 2017
+/*  Created by: 
+ *
+ *      * Hunter Brown
+ *      * Duncan Klug 
+ *      * Andrew Morgan
+ *      * HuiMin Zhang
+ *
+ *  Copyright (c) 2017 Really Good Software
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
 #ifndef WAVE_H
 #define WAVE_H
 
@@ -20,7 +46,7 @@ enemy_map *e;                       // Keep track of all enemies on screen
 pow_map *p;                         // Keep track of all powerups
 
 std::vector<PowerUp*> wavePowerUps; // Fill this vector with all powerups you'd like to spawn during a given wave
-double powerUpCooldown = 10000;     // How often to spawn powerups. Can be changed
+double powerUpCooldown = 10000;     // How often to spawn powerups. Can be changed per wave
 
 bool finishedSpawning = false;      // Set to true when wave is finished
 float ratio = (float) 2 / 3;        // 2/3 ratio for spawning behind boundary line
