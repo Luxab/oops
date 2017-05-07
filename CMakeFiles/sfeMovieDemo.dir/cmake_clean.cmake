c956b09a9a6e2d7536e4d902137625764384f2ac
Andrew Morgan <anoa@openmailbox.org>
2017-05-07 00:35:35 -0700

    Loaded in libraries

---
+++ CMakeFiles/sfeMovieDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfeMovieDemo.dir/game.cpp.o"
+  "sfeMovieDemo.pdb"
+  "sfeMovieDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfeMovieDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
